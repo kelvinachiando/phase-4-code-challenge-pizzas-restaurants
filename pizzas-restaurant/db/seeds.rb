c5d63fbf68b97d0e0724343eb9bb1ac1b2028ca8
kelvinachiando <kelvinostin22gmail.com>
2022-10-07 03:37:30 +0300

    added seeding data

--- pizzas-restaurant/db/seeds.rb
+++ pizzas-restaurant/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#pizzas
+ pizzas = Pizza.create("id": 1, "name": "Cheese", "ingredients": "Dough, Tomato Sauce, Cheese")
+ pizzas = Pizza.create("id": 2, "name": "Pepperoni", "ingredients": "Dough, Tomato Sauce, Pepperoni")
+
+ #restaurants
+ restaurants = Restaurant.create("id": 1, "name": "Sottocasa NYC", "address": "298 Atlantic Ave, Brooklyn, NY 11201")
+ restaurants = Restaurant.create("id": 2, "name": "PizzArte", "address": "69 W 55th St, New York, NY 10019")
+
+ #restaurant_pizzas
+#  restaurant_pizzas = Restaurant_pizzas.create("price": 5, "pizza_id": 1, "restaurant_id": 3)
+#  restaurant_pizzas = Restaurant_pizzas.create("price": 6, "pizza_id": 2, "restaurant_id": 2)
+
+ puts "✅ Done seeding!"
